1eff1bdc7d48a0fd71c596f860e25779d5d4d8c2
dirty <darby.chang@gmail.com>
2023-04-01 22:10:27 +0800

    feat: TrackBot.expand()

--- track-bot.py
+++ track-bot.py
@@ -1,3 +1,4 @@
+import copy
 from heapq import heappop as pop
 from heapq import heappush as push
 
@@ -6,10 +7,38 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-DIRS = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+GOAL = 36
 
-class State():
+DIRS = ["UP", "RIGHT", "DOWN", "LEFT"]
+INVERSE_DIRS = [2, 3, 0, 1]
+ROTATE_90_DIRS = [1, 2, 3, 0]
 
+
+class Track:  # {{{
+    def __init__(self, _id: int, in_dir: int, dx: int, dy: int, out_dir: int):
+        self.id = _id
+        self.in_dir = in_dir
+        self.dx = dx
+        self.dy = dy
+        self.out_dir = out_dir
+
+    def __str__(self):
+        return f"{self.id} {DIRS[self.in_dir]} {self.dx} {self.dy} {DIRS[self.out_dir]}"
+
+    def rotate_90(self) -> object:
+        return Track(
+            self.id,
+            ROTATE_90_DIRS[self.in_dir],
+            -self.dy,
+            self.dx,
+            ROTATE_90_DIRS[self.out_dir],
+        )
+
+
+# }}}
+
+
+class State:
     def __init__(self, x: int, y: int, out_dir: int, score: int = 0):
         self.out_dir = out_dir
         self.score = score
@@ -20,24 +49,39 @@ class State():
         return self.score < other.score
 
     def __str__(self):
-        return f'{self.x} {self.y} {DIRS[self.out_dir]} {self.score}'
+        return f"{self.x} {self.y} {DIRS[self.out_dir]} {self.score}"
 
 
-class Track():
+class TrackBot:
 
-    def __init__(self, in_dir: int, dx: int, dy: int, out_dir: int):
-        self.in_dir = in_dir
-        self.dx = dx
-        self.dy = dy
-        self.out_dir = out_dir
-
-
-class TrackBot():
+    seen_states = {}
 
     def __init__(self, tracks: list, map_height: int = 9, map_width: int = 36):
         self.map_height = map_height
         self.map_width = map_width
-        self.tracks = tracks
+        State.TRACKS = []
+        for track in tracks:
+            State.TRACKS.append(track)
+            for i in range(3):
+                State.TRACKS.append(State.TRACKS[-1].rotate_90())
+
+    def expand(self, state) -> list:
+        states = []
+        for track in State.TRACKS:
+            if track.in_dir != INVERSE_DIRS[state.out_dir]:
+                continue
+            new_state = State(
+                state.x + track.dx,
+                state.y + track.dy,
+                track.out_dir,
+                state.x + track.dx,
+            )
+            if new_state.x < 0:
+                continue
+            if not (0 <= new_state.y < self.map_height):
+                continue
+            states.append(new_state)
+        return states
 
     def play(self):
         heap = []
@@ -48,23 +92,30 @@ class TrackBot():
         for y in range(9):
             push(heap, State(-1, y, RIGHT))
         state = pop(heap)
-        print(state)
-        return
 
         # move
-        while score < GOAL:
+        while state.score < GOAL:
             for child in self.expand(state):
+                print(child)
                 if self.seen(child):
                     continue
-                heap.push([child.score(), child])
-            score, state = heap.pop()
+                push(heap, child)
+            state = heap.pop()
+
+    def seen(self, track: object) -> bool:
+        return False
 
 
-bot = TrackBot([
-    Track(LEFT, 1, 2, LEFT), # 1
-    Track(LEFT, 1, 2, RIGHT), # 2
-    Track(UP, 0, 3, DOWN), # 3
-    Track(LEFT, 1, 2, DOWN), # 4
-    Track(RIGHT, -1, 2, DOWN) # 5
-])
+bot = TrackBot(
+    [
+        Track(1, LEFT, 1, 2, LEFT),
+        Track(1, LEFT, 1, -2, LEFT),
+        Track(2, LEFT, 1, 2, RIGHT),
+        Track(3, UP, 0, 3, DOWN),
+        Track(4, LEFT, 1, 2, DOWN),
+        Track(4, DOWN, 0, -3, LEFT),
+        Track(5, RIGHT, -1, 2, DOWN),
+        Track(5, DOWN, 0, -3, RIGHT),
+    ]
+)
 bot.play()
